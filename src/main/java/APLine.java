6c7465c6204d9e91c010c31720bfccdc14031cbb
Khuslenttt <entegshjargal1@s.sfusd.edu>
2025-04-28 09:17:50 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,16 @@
 public class APLine
 {
   /* your code here */
-  
+  private int a , b , c;
+  public APLIne(int a1, int b1, int c1){
+    a  = a1; 
+    b = b1;
+    c = c1;
+  }
+  public double getSlope(){
+    return -(double)a/b;
+  }
+  public boolean isOneLine(int x , int y){
+    return a*x+b*y+c==0;
+      }
 }
